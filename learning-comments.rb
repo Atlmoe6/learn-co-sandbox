1bb9a50fd5f97120e6172aec532e9ee728a8c2b8
abstract-inheritance-c040 <doe.mahamud1@gmail.com>
2021-05-19 21:35:36 +0000

    Done.

---
+++ learning-comments.rb
@@ -0,0 +1,14 @@
+#From the Three Dog Night Song
+puts "Joy to the world"
+puts "All the boys and girls"
+puts "Joy to the fishes in the deep blue sea"
+puts "Joy to you and me"
+
+
+puts "Joy to the world"
+puts "All the boys and girls"
+# puts "Joy to the fishes in the deep blue sea"
+puts "Joy to you and me"
+
+name = "Mohamed"
+puts "I was wondering if you had recently heard back from #{name} recently"
